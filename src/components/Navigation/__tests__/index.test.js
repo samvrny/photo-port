32ea5ca8238475b8b988f26f7d0d8e91a351070a
samvrny <samvrny@gmail.com>
2022-10-02 20:19:04 -0500

    added navigation bar tests

---
+++ src/components/Navigation/__tests__/index.test.js
@@ -0,0 +1,37 @@
+import { resnder, cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Navigation from '..';
+
+afterEach(cleanup);
+
+describe('Navigation component', () => {
+    //baseline test
+    it('renders', () => {
+        render(<Navigation/>);
+    });
+
+    //snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Navigation/>);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        //arrange
+        const { getByLabelText } = render(<Navigation/>);
+        //assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        //arrange
+        const { getByTestId } = render(<Navigation/>);
+        //assert
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+    });
+})
