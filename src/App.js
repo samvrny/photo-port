16fd9ee5d8b1453027a9b58fef430a28f63df0e7
samvrny <samvrny@gmail.com>
2022-10-02 22:25:24 -0500

    working on react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import About from './components/About';
 import Navigation from './components/Navigation';
+import Gallery from './components/Gallery';
+import { useState } from 'react';
 
 function App() {
+  const [categories] = useState([
+    {
+        name: 'commercial',
+        description: 'Photos of grocery stores, food trucks, and other commercial projects'
+    },
+    {
+        name: 'portraits',
+        description: 'Portraits of people in my life'
+    },
+    {
+        name: 'food',
+        description: 'Delicious delecacies'
+    },
+    {
+        name: 'landscape',
+        description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Navigation></Navigation>
+      <Navigation
+        categories = {categories}
+        setCurrentCategory = {setCurrentCategory}
+        currentCategory = {currentCategory}
+      ></Navigation>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,32 +1,22 @@
-function Navigation() {
-    const categories = [
-        {
-            name: 'commercial',
-            description: 'Photos of grocery stores, food trucks, and other commercial projects'
-        },
-        {
-            name: 'portraits',
-            description: 'Portraits of people in my life'
-        },
-        {
-            name: 'food',
-            description: 'Delicious delecacies'
-        },
-        {
-            name: 'landscape',
-            description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
-        }
-    ];
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import { useEffect } from 'react';
 
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
-    //the header below used to not have the flex row class, or at least the module didn't call for it.
+function Navigation(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
     return (
-        <header className="flex-row"> 
+        <header className="flex-row px-1"> 
             <h2>
                 <a data-testid="link" href='/'>
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera">{" "}📸</span>{" "} Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -40,9 +30,11 @@ function Navigation() {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className="mx-1" key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => {
+                                setCurrentCategory(category)
+                            }}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
